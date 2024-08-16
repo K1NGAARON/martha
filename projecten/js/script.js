f7c50f715e97e1c51648cd32f2de100e4cfe5c23
K1NGAARON <gerardaaron1997@gmail.com>
2024-08-16 14:04:55 +0200

    changes

--- projecten/js/script.js
+++ projecten/js/script.js
@@ -1,59 +1,77 @@
-function addProjects(e) {
-    const projects = [
-        {
-            title: "Project 1",
-            img: "/projecten/bellegem/img/banner.jpg",
-            link: "/projecten/bellegem/",
-        },
-        {
-            title: "Project 2",
-            img: "/projecten/gistel/img/banner.jpg",
-            link: "/projecten/gistel/",
-        },
-        {
-            title: "Project 3",
-            img: "/projecten/kortrijk/img/banner.jpg",
-            link: "/projecten/kortrijk/",
-        },
-        {
-            title: "Project 4",
-            img: "/projecten/kruiskerke/img/banner.jpg",
-            link: "/projecten/kruiskerke/",
-        },
-        {
-            title: "Project 5",
-            img: "/projecten/roeselare/img/banner.jpg",
-            link: "/projecten/roeselare/",
-        },
-    ];
+const projects = [
+    {
+        title: "Project 1",
+        img: "https://www.victorrenoveert.be/files/images/1200x780/victor-woning-moerbeke-waas-aug-2023-01_1.jpg",
+        link: "/projecten/bellegem/",
+        tag: "filter-1",
+    },
+    {
+        title: "Project 2",
+        img: "https://www.victorrenoveert.be/files/images/1200x780/victor-woning-moerbeke-waas-aug-2023-01_1.jpg",
+        link: "/projecten/gistel/",
+        tag: "filter-1",
+    },
+    {
+        title: "Project 3",
+        img: "https://www.victorrenoveert.be/files/images/1200x780/victor-woning-moerbeke-waas-aug-2023-01_1.jpg",
+        link: "/projecten/kortrijk/",
+        tag: "filter-3",
+    },
+    {
+        title: "Project 4",
+        img: "https://www.victorrenoveert.be/files/images/1200x780/victor-woning-moerbeke-waas-aug-2023-01_1.jpg",
+        link: "/projecten/kruiskerke/",
+        tag: "filter-2",
+    },
+    {
+        title: "Project 5",
+        img: "https://www.victorrenoveert.be/files/images/1200x780/victor-woning-moerbeke-waas-aug-2023-01_1.jpg",
+        link: "/projecten/roeselare/",
+        tag: "filter-3",
+    },
+];
 
-    const target = document.querySelector('#project-wrapper');
-
-    if (!target) {
-        return;
-    }
+function createCards(e) {
+    const target = document.querySelector(".projects-wrapper");
 
     const cards = projects.map(item => {
-
         return `
-            <a href="${item.link}" class="item-link">
-                <div class="item">
-                    <img src="${item.img}" alt="${item.title}">
-                    <div class="content">
-                        <h3>
-                            ${item.title}
-                        </h3>
-                        <p class="read-more">
-                            Lees meer
-                        </p>
-                    </div>
-                    <div class="overlay"></div>
+            <div class="item card ${item.tag}">
+                <img src="${item.img}" alt="${item.title}">
+                <div class="content">
+                    <h3>
+                        ${item.title}
+                    </h3>
+                    <p>
+                        ${item.body}
+                    </p>
+                    <a href="${item.url}" class="btn">
+                        Lees meer
+                    </a>
                 </div>
-            </a>
+            </div>
         `;
     }).join('');
 
     target.innerHTML = cards;
-};
+}
 
-$(document).ready(addProjects);
+function filterContent(e) {
+    $('.filter-btn').removeClass('active');
+    $(this).addClass('active');
+
+    $('.projects-wrapper .card').fadeOut();
+
+    let activeFilter = $(this).attr('id');
+
+    if (activeFilter === 'all') {
+        $('.projects-wrapper .card').fadeIn();
+    } else {
+        $('.' + activeFilter).fadeIn();
+    }
+}
+
+$(document).ready(function() {
+    createCards();
+    $('.filter-btn').click(filterContent);
+});
