c2a8c337bf83802032fd62c25e280767be53af27
K1NGAARON <gerardaaron1997@gmail.com>
2024-08-16 18:38:36 +0200

    changes

---
+++ vacatures/js/script.js
@@ -0,0 +1,74 @@
+const projects = [
+    {
+        title: "Bouwvakker met ervaring in xyz",
+        img: "https://www.victorrenoveert.be/files/images/1200x780/victor-woning-moerbeke-waas-aug-2023-01_1.jpg",
+        link: "/projecten/bellegem/",
+        tag: "job-3",
+    },
+    {
+        title: "Bouwvakker met ervaring in xyz",
+        img: "https://www.victorrenoveert.be/files/images/1200x780/victor-woning-moerbeke-waas-aug-2023-01_1.jpg",
+        link: "/projecten/gistel/",
+        tag: "job-2",
+    },
+    {
+        title: "Bouwvakker met ervaring in xyz",
+        img: "https://www.victorrenoveert.be/files/images/1200x780/victor-woning-moerbeke-waas-aug-2023-01_1.jpg",
+        link: "/projecten/kortrijk/",
+        tag: "job-3",
+    },
+    {
+        title: "Bouwvakker met ervaring in xyz",
+        img: "https://www.victorrenoveert.be/files/images/1200x780/victor-woning-moerbeke-waas-aug-2023-01_1.jpg",
+        link: "/projecten/kruiskerke/",
+        tag: "job-1",
+    },
+    {
+        title: "Bouwvakker met ervaring in xyz",
+        img: "https://www.victorrenoveert.be/files/images/1200x780/victor-woning-moerbeke-waas-aug-2023-01_1.jpg",
+        link: "/projecten/roeselare/",
+        tag: "job-1",
+    },
+];
+
+function createCards(e) {
+    const target = document.querySelector(".projects-wrapper");
+
+    const cards = projects.map(item => {
+        return `
+            <a class="project ${item.tag}" href="${item.url}">
+                <div class="image-wrapper">
+                    <img src="${item.img}" alt="${item.title}">
+                </div>
+                <div class="content">
+                    <p class="heading">
+                        <span class="tag">${item.tag}</span> | 
+                        <span class="title">${item.title}</span>
+                    </p>
+                </div>
+            </a>
+        `;
+    }).join('');
+
+    target.innerHTML = cards;
+}
+
+function filterContent(e) {
+    $('.filter-btn').removeClass('active');
+    $(this).addClass('active');
+
+    $('.projects-wrapper .project').fadeOut();
+
+    let activeFilter = $(this).attr('id');
+
+    if (activeFilter === 'all') {
+        $('.projects-wrapper .project').fadeIn();
+    } else {
+        $('.' + activeFilter).fadeIn();
+    }
+}
+
+$(document).ready(function() {
+    createCards();
+    $('.filter-btn').click(filterContent);
+});
